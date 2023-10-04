dcd44e6e8ba31d58c02a38fc38e0dde141bf41b6
Jose <jgh9094@gmail.com>
2023-10-04 15:01:15 -0700

    get rng from base_evolver and use it to change offspring

--- tpot2/population.py
+++ tpot2/population.py
@@ -12,43 +12,47 @@ import copy
 import pickle
 import dask
 
-def mutate(individual):
+def mutate(individual, rng_):
+    rng = np.random.default_rng(rng_)
     if isinstance(individual, collections.abc.Iterable):
         for ind in individual:
-            ind.mutate()
+            ind.mutate(rng)
     else:
-        individual.mutate()
+        individual.mutate(rng)
     return individual
 
-def crossover(parents):
-    parents[0].crossover(parents[1])
+def crossover(parents, rng_):
+    rng = np.random.default_rng(rng_)
+    parents[0].crossover(parents[1],rng)
     return parents[0]
 
-def mutate_and_crossover(parents):
-    parents[0].crossover(parents[1])
-    parents[0].mutate()
-    parents[1].mutate()
+def mutate_and_crossover(parents, rng_):
+    rng = np.random.default_rng(rng_)
+    parents[0].crossover(parents[1],rng)
+    parents[0].mutate(rng)
+    parents[1].mutate(rng)
     return parents
 
-def crossover_and_mutate(parents):
+def crossover_and_mutate(parents, rng_):
+    rng = np.random.default_rng(rng_)
     for p in parents:
-        p.mutate()
-    parents[0].crossover(parents[1])
+        p.mutate(rng)
+    parents[0].crossover(parents[1],rng)
     return parents[0]
 
 
-built_in_var_ops_dict = {"mutate":mutate, 
-                        "crossover":crossover, 
-                        "mutate_then_crossover":mutate_and_crossover, 
+built_in_var_ops_dict = {"mutate":mutate,
+                        "crossover":crossover,
+                        "mutate_then_crossover":mutate_and_crossover,
                         "crossover_then_mutate":crossover_and_mutate}
 
 
 
-    
+
 class Population():
     '''
     Primary usage is to keep track of evaluated individuals
-    
+
     Parameters
     ----------
     initial_population : {list of BaseIndividuals}, default=None
@@ -59,7 +63,7 @@ class Population():
     callback : {function}, default=None
         NOT YET IMPLEMENTED
         A function to call after each generation. The function should take a Population object as its only argument.
-    
+
     Attributes
     ----------
     population : {list of BaseIndividuals}
@@ -75,7 +79,7 @@ class Population():
                     ) -> None:
 
         if column_names is not None:
-            
+
             column_names = column_names+["Parents", "Variation_Function"]
         else:
             column_names = ["Parents", "Variation_Function"]
@@ -94,12 +98,12 @@ class Population():
     def remove_invalid_from_population(self, column_names, invalid_value = "INVALID"):
         '''
         Remove individuals from the live population if either do not have a value in the column_name column or if the value contains np.nan.
-        
+
         Parameters
         ----------
         column_name : {str}
             The name of the column to check for np.nan values.
-        
+
         Returns
         -------
         None
@@ -110,17 +114,17 @@ class Population():
         is_valid = lambda ind: ind.unique_id() not in self.evaluated_individuals.index or invalid_value not in self.evaluated_individuals.loc[ind.unique_id(),column_names].to_list()
         self.population = [ind for ind in self.population if is_valid(ind)]
 
-        
 
-    # takes the list of individuals and adds it to the live population list. 
+
+    # takes the list of individuals and adds it to the live population list.
     # if keep_repeats is False, repeated individuals are not added to the population
-    # returns a list of individuals added to the live population  
+    # returns a list of individuals added to the live population
     #TODO make keep repeats allow for previously evaluated individuals,
     #but make sure that the live population only includes one of each, no repeats
-    def add_to_population(self, individuals: typing.List[BaseIndividual], keep_repeats=False, mutate_until_unique=True):
+    def add_to_population(self, individuals: typing.List[BaseIndividual], rng_, keep_repeats=False, mutate_until_unique=True):
         '''
         Add individuals to the live population. Add individuals to the evaluated_individuals if they are not already there.
-        
+
         Parameters:
         -----------
         individuals : {list of BaseIndividuals}
@@ -129,6 +133,8 @@ class Population():
             If True, allow the population to have repeated individuals.
             If False, only add individuals that have not yet been added to geneology.
         '''
+        rng = np.random.default_rng(rng_)
+
         if not isinstance(individuals, collections.abc.Iterable):
             individuals = [individuals]
 
@@ -150,7 +156,7 @@ class Population():
                 elif mutate_until_unique: #If its old and we don't want repeats, we can optionally mutate it until it is unique
                     for _ in range(20):
                         individual = copy.deepcopy(individual)
-                        individual.mutate()
+                        individual.mutate(rng)
                         key = individual.unique_id()
                         if key not in self.evaluated_individuals.index:
                             self.evaluated_individuals.loc[key] = np.nan
@@ -158,7 +164,7 @@ class Population():
                             self.population.append(individual)
                             new_individuals.append(individual)
                             break
-                    
+
         return new_individuals
 
 
@@ -181,7 +187,7 @@ class Population():
 
         self.evaluated_individuals.loc[key,column_names] = data
 
-    
+
     def get_column(self, individual, column_names=None, to_numpy=True):
         '''
         Update the column_name column in the evaluated_individuals with the data.
@@ -215,13 +221,13 @@ class Population():
     def get_unevaluated_individuals(self, column_names, individual_list=None):
         if individual_list is None:
             individual_list = self.population
-        
+
         if self.use_unique_id:
             unevaluated_filter = lambda individual: individual.unique_id() not in self.evaluated_individuals.index or any(self.evaluated_individuals.loc[individual.unique_id(), column_names].isna())
         else:
             unevaluated_filter = lambda individual: individual not in self.evaluated_individuals.index or any(self.evaluated_individuals.loc[individual.unique_id(), column_names].isna())
-        
-        return [individual for individual in individual_list if unevaluated_filter(individual)]    
+
+        return [individual for individual in individual_list if unevaluated_filter(individual)]
 
     # def get_valid_evaluated_individuals_df(self, column_names_to_check, invalid_values=["TIMEOUT","INVALID"]):
     #     '''
@@ -230,18 +236,19 @@ class Population():
     #     return self.evaluated_individuals[~self.evaluated_individuals[column_names_to_check].isin(invalid_values).any(axis=1)]
 
     #the live population empied and is set to new_population
-    def set_population(self,  new_population, keep_repeats=True):
+    def set_population(self,  new_population, rng_, keep_repeats=True):
         '''
         sets population to new population
         for selection?
         '''
         self.population = []
-        self.add_to_population(new_population, keep_repeats=keep_repeats)
+        rng = np.random.default_rng(rng_)
+        self.add_to_population(new_population, rng_=rng, keep_repeats=keep_repeats)
 
-    #TODO should we just generate one offspring per crossover? 
-    def create_offspring(self, parents_list, var_op_list, add_to_population=True, keep_repeats=False, mutate_until_unique=True, n_jobs=1):
+    #TODO should we just generate one offspring per crossover?
+    def create_offspring(self, parents_list, var_op_list, rng_, add_to_population=True, keep_repeats=False, mutate_until_unique=True, n_jobs=1):
         '''
-        parents_list: a list of lists of parents. 
+        parents_list: a list of lists of parents.
         var_op_list: a list of var_ops to apply to each list of parents. Should be the same length as parents_list.
 
         for example:
@@ -251,83 +258,87 @@ class Population():
         This will apply crossover to parent1 and parent2 and mutate to parent3.
 
         Creates offspring from parents using the var_op_list.
-        If string, will use a built in method 
+        If string, will use a built in method
             - "crossover" : crossover
             - "mutate" : mutate
             - "mutate_and_crossover" : mutate_and_crossover
             - "cross_and_mutate" : cross_and_mutate
         '''
+        rng = np.random.default_rng(rng_)
         new_offspring = []
-        all_offspring = parallel_create_offspring(parents_list, var_op_list, n_jobs=n_jobs)
+        all_offspring = parallel_create_offspring(parents_list, var_op_list, rng_=rng, n_jobs=n_jobs)
 
         for parents, offspring, var_op in zip(parents_list, all_offspring, var_op_list):
-            
+
             # if var_op in built_in_var_ops_dict:
             #     var_op = built_in_var_ops_dict[var_op]
 
             # offspring = copy.deepcopy(parents)
             # offspring = var_op(offspring)
             # if isinstance(offspring, collections.abc.Iterable):
-            #     offspring = offspring[0] 
+            #     offspring = offspring[0]
 
             if add_to_population:
-                added = self.add_to_population(offspring, keep_repeats=keep_repeats, mutate_until_unique=mutate_until_unique)
+                added = self.add_to_population(offspring, rng_=rng, keep_repeats=keep_repeats, mutate_until_unique=mutate_until_unique)
                 if len(added) > 0:
                     for new_child in added:
                         parent_keys = [parent.unique_id() for parent in parents]
                         if not pd.api.types.is_object_dtype(self.evaluated_individuals["Parents"]): #TODO Is there a cleaner way of doing this? Not required for some python environments?
                             self.evaluated_individuals["Parents"] = self.evaluated_individuals["Parents"].astype('object')
                         self.evaluated_individuals.at[new_child.unique_id(),"Parents"] = tuple(parent_keys)
-                        
+
                         #if var_op is a function
                         if hasattr(var_op, '__call__'):
                             self.evaluated_individuals.at[new_child.unique_id(),"Variation_Function"] = var_op.__name__
                         else:
                             self.evaluated_individuals.at[new_child.unique_id(),"Variation_Function"] = var_op
-                        
-                        
+
+
                         new_offspring.append(new_child)
 
             else:
                 new_offspring.append(offspring)
-                            
-            
+
+
         return new_offspring
 
-   
+
 
 def get_id(individual):
     return individual.unique_id()
 
-def parallel_create_offspring(parents_list, var_op_list, n_jobs=1):
+def parallel_create_offspring(parents_list, var_op_list, rng_, n_jobs=1):
+    rng = np.random.default_rng(rng_)
     if n_jobs == 1:
-        return nonparallel_create_offpring(parents_list, var_op_list)
+        return nonparallel_create_offpring(parents_list, var_op_list, rng_=rng)
     else:
         delayed_offspring = []
         for parents, var_op in zip(parents_list,var_op_list):
             #TODO put this loop in population class
             if var_op in built_in_var_ops_dict:
                 var_op = built_in_var_ops_dict[var_op]
-            delayed_offspring.append(dask.delayed(copy_and_change)(parents, var_op))
+            delayed_offspring.append(dask.delayed(copy_and_change)(parents, var_op)) # TODO: need to create mini rngs to pass to dask
 
         offspring = dask.compute(*delayed_offspring,
                                 num_workers=n_jobs, threads_per_worker=1)
         return offspring
 
-def nonparallel_create_offpring(parents_list, var_op_list, n_jobs=1):
+def nonparallel_create_offpring(parents_list, var_op_list, rng_, n_jobs=1):
+    rng = np.random.default_rng(rng_)
     offspring = []
     for parents, var_op in zip(parents_list,var_op_list):
         #TODO put this loop in population class
         if var_op in built_in_var_ops_dict:
             var_op = built_in_var_ops_dict[var_op]
-        offspring.append(copy_and_change(parents, var_op))
+        offspring.append(copy_and_change(parents, var_op, rng))
 
     return offspring
 
 
-def copy_and_change(parents, var_op):
+def copy_and_change(parents, var_op, rng_):
+    rng = np.random.default_rng(rng_)
     offspring = copy.deepcopy(parents)
-    offspring = var_op(offspring)
+    offspring = var_op(offspring,rng)
     if isinstance(offspring, collections.abc.Iterable):
         offspring = offspring[0]
     return offspring
